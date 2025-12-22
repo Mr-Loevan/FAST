fa32c0e34e0f15365747e203f3be3fbbcf4cdcd0
Wenyi Xiao <wenyixiao@zju.edu.cn>
2025-12-23 01:55:59 +0800

    Fix config path in train_fast_llm.sh script

--- examples/train_fast_llm.sh
+++ examples/train_fast_llm.sh
@@ -6,7 +6,7 @@ MODEL_PATH=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B  # replace it with your loc
 TRAIN_FILE=hiyouga/math12k # replace it with your local file path
 
 python -m verl.trainer.main \
-    config=/g22551154lrr/FAST-GRPO-main-69dac76506ba3e2fe2d2ae2387f594d83e4d3124/FAST-GRPO-main-69dac76506ba3e2fe2d2ae2387f594d83e4d3124/examples/config2_kl_group.yaml \
+    config=examples/config.yaml \
     data.train_files=${TRAIN_FILE} \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=fast-grpo-llm \
